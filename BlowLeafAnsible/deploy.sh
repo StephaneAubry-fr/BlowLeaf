af56f1be2b50328109237bb8f63fc4369cef7282
Stéphane Aubry <stephane.aubry.5783@gmail.com>
2025-04-16 18:50:41 +0200

    BlowLeafAnsible - Jenkins ssh deployer

---
+++ BlowLeafAnsible/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf ~/jenkins-deploy
+mkdir ~/jenkins-deploy
+
+cd ~/jenkins-deploy
+git clone -b main --depth 1 --single-branch git@github.com:StephaneAubry-fr/BlowLeaf.git ./
+
+cd BlowLeafAnsible
+ansible-playbook -i inventory.ini playbook-blowleaf.yml
